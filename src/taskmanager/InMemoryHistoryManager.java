58abe37412afb9d63d87fd8748f3ae8868c48852
Арутюнян Маис <maisxarb@mail.ru>
2025-01-12 22:15:10 +0500

    внес в класс InMemoryHistoryManager необходимые коррективы

--- src/taskmanager/InMemoryHistoryManager.java
+++ src/taskmanager/InMemoryHistoryManager.java
@@ -6,14 +6,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
+
+    private static final int MAX_HISTORY_SIZE = 10;
     private final List<Task> history = new ArrayList<>();
 
     @Override
     public void add(Task task) {
-        history.add(task);
-        if (history.size() > 10) {
+        if (task == null) {
+            throw new IllegalArgumentException("Task cannot be null");
+        }
+
+        if (history.size() >= MAX_HISTORY_SIZE) {
             history.remove(0);
         }
+
+        history.add(task);
     }
 
     @Override
@@ -21,3 +28,6 @@ public class InMemoryHistoryManager implements HistoryManager {
         return new ArrayList<>(history);
     }
 }
+// Подскажите тесты ведь ломают мой проект, я думал вообше габелла будет. У меня же не запускается ничего.
+//И по тестам я все сделал по инструкции, но отдельной папаки как SRС только Test не появляется.
+//Я максимально запутался. Если вы можете, обьясните пожалуйста, что мне делать чтобы все грамотно было.
