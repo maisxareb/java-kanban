50881523a15a7c5c03173a7fa364dc597b56fe8d
Арутюнян Маис <maisxarb@mail.ru>
2025-02-16 14:25:17 +0500

    Добавить функционал в класс InMemoryHistoryManager, изменил интерфейс HistoryManager + тест

--- test/InMemoryTaskManagerTest.java
+++ test/InMemoryTaskManagerTest.java
@@ -1,5 +1,3 @@
-package tests;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
--- test/TaskTest.java
+++ test/TaskTest.java
@@ -1,5 +1,3 @@
-package tests;
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import task.Task;
