e4e7851f78e675b32062cff994bb89c02fd58ed6
Арутюнян Маис <maisxarb@mail.ru>
2025-02-16 13:53:06 +0500

    Добавить функционал в класс InMemoryHistoryManager, изменил интерфейс HistoryManager + тест

--- src/taskmanager/TaskManager.java
+++ src/taskmanager/TaskManager.java
@@ -9,16 +9,28 @@ import java.util.List;
 
 public interface TaskManager {
     Task createTask(Task task);
+
     Epic createEpic(Epic epic);
+
     Subtask createSubtask(Subtask subtask);
+
     Task getTask(int id);
+
     Epic getEpic(int id);
+
     Subtask getSubtask(int id);
+
     void removeTask(int id);
+
     void removeEpic(int id);
+
     void removeSubtask(int id);
+
     Collection<Task> getAllTasks();
+
     Collection<Epic> getAllEpics();
+
     Collection<Subtask> getAllSubtasks();
+
     List<Task> getHistory();
 }
