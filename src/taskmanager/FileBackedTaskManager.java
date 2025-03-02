ba6c0635b99462abb46ed40ed3d7c725c2d6cad4
Арутюнян Маис <maisxarb@mail.ru>
2025-03-03 00:02:53 +0500

    Добавлены функции для расчета продолжительности и проверки пересечений задач

--- src/taskmanager/FileBackedTaskManager.java
+++ src/taskmanager/FileBackedTaskManager.java
@@ -13,8 +13,6 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
 
